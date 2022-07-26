08029943add161da1bd5b7e4df8eef7b39e5234a
21020029 <hoanghoangminhtu1708@gmail.com>
2022-07-26 22:44:26 +0700

    add distribution

--- FAWPredict.py
+++ FAWPredict.py
@@ -129,9 +129,25 @@ if __name__ == "__main__":
     for i in range(len(sys.argv)):
         if(sys.argv[i] == "--mode"): cal_mode = sys.argv[i + 1]
         if(sys.argv[i] == "--location"): location = sys.argv[i + 1]
-        if(sys.argv[i] == "--date"): cur_time = sys.argv[i + 1]
-        if(sys.argv[i] == "--age"): cur_age = int(sys.argv[i + 1])
-
+        if(sys.argv[i] == "--date"): start_date = sys.argv[i + 1]
+        if(sys.argv[i] == "--age"): 
+            cur_age = 0
+            tmp = sys.argv[i + 1].split(',')
+            if(len(tmp) == 1):
+                cur_age = int(tmp[0])
+                break
+            age_arr = []
+            for age in tmp:
+                age_tmp = age.split(':')
+                if(len(age_tmp) != 2):
+                    print("Wrong format distribution")
+                    sys.exit()
+                age_arr.append((int(age_tmp[0]), float(age_tmp[1])))
+            
+            max_distribution = 0
+            for i in range(len(age_arr)):
+                if(max_distribution > age_arr[i][1]):
+                    cur_age = age_arr[i][0]
     weather_table_api = API_Reading(base_url, api_key, location, start_date, end_date, unit_group, content_type, include)
     weather_table_api.create_api()
     weather_table = weather_table_api.run_api()
